dde767218724210f0cc7a77d929670ae33d6fc6e
Kehvarl <kehvarl@kehvarl.com>
2019-11-09 19:42:41 -0700

    Abstract view, and set up initial model

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -9,19 +9,35 @@ import Html.Attributes exposing (..)
 
 
 main =
-    view "no model yet"
+    view initialModel
+
+
+
+--MODEL
+
+
+initialModel =
+    [ { url = "1.jpeg" }
+    , { url = "2.jpeg" }
+    , { url = "3.jpeg" }
+    ]
 
 
 
 --VIEW
 
 
+urlPrefix =
+    "https://elm-in-action.com/"
+
+
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumb-nails" ]
-            [ img [ src "https://elm-in-action.com/1.jpeg" ] []
-            , img [ src "https://elm-in-action.com/2.jpeg" ] []
-            , img [ src "https://elm-in-action.com/3.jpeg" ] []
-            ]
+            (List.map viewThumbnail model)
         ]
+
+
+viewThumbnail thumb =
+    img [ src (urlPrefix ++ thumb.url) ] []
