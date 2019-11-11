1472de866e4373b18aff68465ec5396269286132
Kehvarl <kehvarl@kehvarl.com>
2019-11-10 17:59:59 -0700

    Refactoring Adding Type Annotations Custom Type Aliases Surprise Me button

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,5 +1,6 @@
 module PhotoGroove exposing (main)
 
+import Array exposing (Array)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -22,6 +23,17 @@ main =
 --MODEL
 
 
+type alias Photo =
+    { url : String }
+
+
+type alias Model =
+    { photos : List Photo
+    , selectedUrl : String
+    }
+
+
+initialModel : Model
 initialModel =
     { photos =
         [ { url = "1.jpeg" }
@@ -32,29 +44,48 @@ initialModel =
     }
 
 
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
+
 
 --UPDATE
 
 
-update msg model =
-    if msg.description == "ClickedPhoto" then
-        { model | selectedUrl = msg.data }
+type alias Msg =
+    { description : String, data : String }
 
-    else
-        model
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg.description of
+        "ClickedPhoto" ->
+            { model | selectedUrl = msg.data }
+
+        "ClickedSurpriseMe" ->
+            { model | selectedUrl = "2.jpeg" }
+
+        _ ->
+            model
 
 
 
 --VIEW
 
 
+urlPrefix : String
 urlPrefix =
     "https://elm-in-action.com/"
 
 
+view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
+        , button
+            [ onClick { description = "ClickedSurpriseMe", data = "" } ]
+            [ text "Surprise Me" ]
         , div [ id "thumb-nails" ]
             (List.map (viewThumbnail model.selectedUrl) model.photos)
         , img
@@ -65,6 +96,7 @@ view model =
         ]
 
 
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumb =
     img
         [ src (urlPrefix ++ thumb.url)
