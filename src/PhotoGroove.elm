c19fbdfc7e24eab649e201c51cb1acc9481e9b20
Kehvarl <kehvarl@kehvarl.com>
2019-11-10 17:41:23 -0700

    Chapter 2 onClick solution

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,7 +1,9 @@
 module PhotoGroove exposing (main)
 
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 
 
@@ -9,7 +11,11 @@ import Html.Attributes exposing (..)
 
 
 main =
-    view initialModel
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
 
 
 
@@ -27,6 +33,18 @@ initialModel =
 
 
 
+--UPDATE
+
+
+update msg model =
+    if msg.description == "ClickedPhoto" then
+        { model | selectedUrl = msg.data }
+
+    else
+        model
+
+
+
 --VIEW
 
 
@@ -38,7 +56,7 @@ view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumb-nails" ]
-            (List.map (\photo -> viewThumbnail model.selectedUrl photo) model.photos)
+            (List.map (viewThumbnail model.selectedUrl) model.photos)
         , img
             [ class "large"
             , src (urlPrefix ++ "large/" ++ model.selectedUrl)
@@ -51,5 +69,6 @@ viewThumbnail selectedUrl thumb =
     img
         [ src (urlPrefix ++ thumb.url)
         , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        , onClick { description = "ClickedPhoto", data = thumb.url }
         ]
         []
