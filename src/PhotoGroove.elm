fd1519b77db18be13aaca39ab6e9be2919475803
Kehvarl <kehvarl@kehvarl.com>
2019-11-19 18:16:43 -0700

    Convert options to Pasta from ints to floats.

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -68,7 +68,7 @@ type alias Model =
 
 type alias FilterOptions =
     { url : String
-    , filters : List { name : String, amount : Int }
+    , filters : List { name : String, amount : Float }
     }
 
 
@@ -134,9 +134,9 @@ applyFilters model =
         Loaded photos selectedUrl ->
             let
                 filters =
-                    [ { name = "Hue", amount = model.hue }
-                    , { name = "Ripple", amount = model.ripple }
-                    , { name = "Noise", amount = model.noise }
+                    [ { name = "Hue", amount = toFloat model.hue / 11 }
+                    , { name = "Ripple", amount = toFloat model.ripple / 11 }
+                    , { name = "Noise", amount = toFloat model.noise / 11 }
                     ]
 
                 url =
